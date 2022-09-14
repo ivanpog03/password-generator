6d87feded52470c48be91f0decb1fbad78ce8f7e
pogadaevi <pogadaevi@bzz.ch>
2022-09-14 19:48:59 +0200

    home and password

--- generator/views.py
+++ generator/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+
+def home(request):
+    # use templates from templates folder with render-fucntion
+
+    return render(request, 'generator/home.html')
+
+
+def password(request):
+    return render(request, 'generator/password.html')
